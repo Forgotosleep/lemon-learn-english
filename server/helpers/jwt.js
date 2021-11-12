00bb7b5302d9d8d64cc608313a3ac435c58c1a85
dionkuarta97 <diondpkp97@gmail.com>
2021-11-12 17:39:58 +0700

    udah jadi yak login nya

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,12 +1,13 @@
-const jwt = require('jsonwebtoken');
-const secret = process.env.SECRETKEY
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRETKEY;
 
 const createToken = (payload) => {
-  return jwt.sign(payload, secret)
-}
+  return jwt.sign(payload, secret);
+  console.log(secret);
+};
 
 const verifyToken = (token) => {
-  return jwt.verify(token, secret)
-}
+  return jwt.verify(token, secret);
+};
 
-module.exports = { createToken, verifyToken }
+module.exports = { createToken, verifyToken };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const route = express.Router();
-
+const UsersController = require("../controllers/UsersController");
 const UserRouter = require("./UserRouter");
 const ClassRouter = require("./ClassRouter");
+const errorHandler = require("../middlewares/errorHandler");
+const authentication = require("../middlewares/authentication");
 
+route.post("/register", UsersController.newUser);
+route.post("/login", UsersController.login);
+route.use(authentication);
 route.use("/users", UserRouter);
 route.use("/classes", ClassRouter);
-
+route.use(errorHandler);
 module.exports = route;
