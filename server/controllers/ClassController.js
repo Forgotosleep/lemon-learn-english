49c8842561d9a91d7b82a2ebe5d92d89e1d128db
Dion Kuarta <ionarta@NS14A6-5e38e15e.localdomain>
2021-11-12 21:43:05 +0700

    add authorization

--- server/controllers/ClassController.js
+++ server/controllers/ClassController.js
@@ -1,97 +1,102 @@
-const { Class } = require('../models')
-
+const { Class } = require("../models");
+const { getPagingData } = require("../helpers/pagination");
 class ClassController {
   static async findAllClass(req, res, next) {
     try {
-      const result = await Class.findAll()
-      res.status(200).json(result)
+      const result = await Class.findAll();
+      res.status(200).json(result);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   static async findOneClass(req, res, next) {
     try {
-      const { id } = req.params
-      const result = await Class.findOne(id)
+      const { id } = req.params;
+      const result = await Class.findOne(id);
 
-      if (!result) {  // IF CLASS NOT FOUND
-        throw ({ name: 'ClassNotFound', id })
+      if (!result) {
+        // IF CLASS NOT FOUND
+        throw { name: "ClassNotFound", id };
       }
 
       // IF CLASS IS FOUND
-      res.status(200).json(result)
-
+      res.status(200).json(result);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   static async addClass(req, res, next) {
     try {
-      const { name, teacherId, levelId, categoryId, ratings } = req.body
+      const { name, teacherId, levelId, categoryId, ratings } = req.body;
 
-      const result = await Class.create(
-        {
-          name, teacherId, levelId, categoryId, ratings
-        },
-      )
+      const result = await Class.create({
+        name,
+        teacherId,
+        levelId,
+        categoryId,
+        ratings,
+      });
 
       // IF CLASS IS CREATED
-      res.status(201).json(result)
-
+      res.status(201).json(result);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   static async updateClass(req, res, next) {
     try {
-      const { id } = req.params
-      const { name, teacherId, levelId, categoryId, ratings } = req.body
+      const { id } = req.params;
+      const { name, teacherId, levelId, categoryId, ratings } = req.body;
       const result = await Class.update(
         {
-          name, teacherId, levelId, categoryId, ratings
+          name,
+          teacherId,
+          levelId,
+          categoryId,
+          ratings,
         },
         {
           where: { id },
-          returning: true
+          returning: true,
         }
-      )
+      );
 
       // IF CLASS NOT FOUND
       if (!result[0]) {
-        throw ({ name: 'ClassNotFound', id })
+        throw { name: "ClassNotFound", id };
       }
 
       // IF CLASS IS UPDATED
-      res.status(200).json(result[1])
-
+      res.status(200).json(result[1]);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   static async deleteClass(req, res, next) {
     try {
-      const { id } = req.params
-      const result = await Class.findOne(id)
+      const { id } = req.params;
+      const result = await Class.findOne(id);
 
-      if (!result) {  // IF CLASS NOT FOUND
-        throw ({ name: 'ClassNotFound', id })
+      if (!result) {
+        // IF CLASS NOT FOUND
+        throw { name: "ClassNotFound", id };
       }
 
       // IF CLASS IS FOUND
       const destroyed = Class.destroy({
-        where: { id }
-      })
-      res.status(200).json({ message: `Successfully deleted Class ${result.name}` })
-
+        where: { id },
+      });
+      res
+        .status(200)
+        .json({ message: `Successfully deleted Class ${result.name}` });
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
-
 }
 
-module.exports = ClassController
+module.exports = ClassController;
---
+++ server/middlewares/authorization.js
@@ -0,0 +1,41 @@
+const authorizationAdmin = async (req, res, next) => {
+  try {
+    const role = req.user.role;
+    if (role !== "admin") {
+      throw { name: "Unauthorized" };
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const authorizationTeacher = async (req, res, next) => {
+  try {
+    const role = req.user.role;
+    if (role !== "teacher") {
+      throw { name: "Unauthorized" };
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+const authorizationStudent = async (req, res, next) => {
+  try {
+    const role = req.user.role;
+    if (role !== "Student") {
+      throw { name: "Unauthorized" };
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  authorizationAdmin,
+  authorizationStudent,
+  authorizationTeacher,
+};
