7e490b27662415e20f5f65a97aa6cd8e12678d90
Dion Kuarta <ionarta@NS14A6-5e38e15e.localdomain>
2021-11-13 18:17:39 +0700

    push dulu

--- server/routes/ClassRouter.js
+++ server/routes/ClassRouter.js
@@ -2,12 +2,16 @@ const express = require("express");
 const route = express.Router();
 
 const ClassController = require("../controllers/ClassController");
-const { authorizationTeacher } = require("../middlewares/authorization");
+const { authorizationTeacher, authorizationStudent } = require("../middlewares/authorization");
 
 route.get("/", ClassController.findAllClass);
+route.get("/teacherClasses", ClassController.findClassByTeacherId);
+route.get("/active", ClassController.findActiveClass);
 route.get("/:id", ClassController.findOneClass);
 route.post("/", authorizationTeacher, ClassController.addClass);
 route.put("/:id", ClassController.updateClass);
 route.delete("/:id", ClassController.deleteClass);
+route.patch("/status/:id", authorizationTeacher, ClassController.updateStatusClass);
+route.patch("/:id", authorizationStudent, ClassController.rateClass);
 
 module.exports = route;
