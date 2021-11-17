bb60a0a10bd057895507c92b5515ddc31159f494
joelpanjaitan <metrohal88@gmail.com>
2021-11-18 04:10:58 +0700

    Login google

--- client-mobile/src/pages/LoginPage.js
+++ client-mobile/src/pages/LoginPage.js
@@ -1,62 +1,113 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom';
-import { fetchLogin } from '../store/actions/actionUser';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchLogin, googleLogin } from "../store/actions/actionUser";
+import GoogleLogin from "react-google-login";
 function LoginPage() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [loginData, setLoginData] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
 
   const changeHandling = (e) => {
-    const value = e.target.value
-    const name = e.target.name
+    const value = e.target.value;
+    const name = e.target.name;
     setLoginData({
       ...loginData,
-      [name]: value
-    })
-  }
-  
+      [name]: value,
+    });
+  };
+
   const submitLogin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(fetchLogin(loginData))
       .then(() => {
-        navigate('/')
+        navigate("/");
       })
       .catch((err) => {
-        console.log(err)
+        console.log(err);
+      });
+  };
+  const responseGoogle = (response) => {
+    // response.preventDefault();
+    const payload = response.tokenId;
+    // console.log(response.tokenId);
+    // console.log(response.profileObj);
+    dispatch(googleLogin(payload))
+      .then(() => {
+        navigate("/");
       })
-  }
-
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
-      <div className='mx-auto' style={{ borderRadius: '20px', textAlign: 'center', color: 'red', maxWidth: '800px' }}>
+      <div
+        className="mx-auto"
+        style={{
+          borderRadius: "20px",
+          textAlign: "center",
+          color: "red",
+          maxWidth: "800px",
+        }}
+      >
         <div className="card-body m-auto d-flex flex-column">
           <center>
-            <img src="https://raw.githubusercontent.com/Forgotosleep/lemon-learn-english/developer/assets/logo%20v1.png" width="300" alt="" />
+            <img
+              src="https://raw.githubusercontent.com/Forgotosleep/lemon-learn-english/developer/assets/logo%20v1.png"
+              width="300"
+              alt=""
+            />
           </center>
           <form onSubmit={submitLogin}>
             <div className="mb-3">
-              <input type="text" onChange={changeHandling} name="email" className="form-control" style={{ borderRadius: 75 }} placeholder="Email" />
+              <input
+                type="text"
+                onChange={changeHandling}
+                name="email"
+                className="form-control"
+                style={{ borderRadius: 75 }}
+                placeholder="Email"
+              />
             </div>
             <div className="mb-3">
-              <input type="password" onChange={changeHandling} name="password" className="form-control" style={{ borderRadius: 75 }} placeholder="Password" />
+              <input
+                type="password"
+                onChange={changeHandling}
+                name="password"
+                className="form-control"
+                style={{ borderRadius: 75 }}
+                placeholder="Password"
+              />
             </div>
-            <button type="submit" className="btn btn-warning mt-2">Submit</button>
+            <button type="submit" className="btn btn-warning mt-2">
+              Submit
+            </button>
           </form>
           <br />
-          <div className="row" style={{ paddingLeft: "5%", paddingRight: "5%" }}>
+          <div
+            className="row"
+            style={{ paddingLeft: "5%", paddingRight: "5%" }}
+          >
             <hr className="col-4 mt-2" />
             <p className="col-4">OR</p>
             <hr className="col-4 mt-2" />
           </div>
-          LOGIN WITH GOOGLE
+          <div class="mb-3">
+            <GoogleLogin
+              clientId="983652955158-5t37lg7gsiq6sniiivmmgqdc6hlbg9ri.apps.googleusercontent.com"
+              buttonText="Google Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
         </div>
-      </div >
+      </div>
     </>
-
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4001;
